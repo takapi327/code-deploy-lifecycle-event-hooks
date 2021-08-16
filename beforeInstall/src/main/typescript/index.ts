45396a93b23c36fb5ee0c0138c5fbac9da47deb1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-08-16 22:44:12 +0900

    Fix index.ts

--- beforeInstall/src/main/typescript/index.ts
+++ beforeInstall/src/main/typescript/index.ts
@@ -5,8 +5,23 @@
 
 import { CodeDeploy } from 'aws-sdk'
 
-const codedeploy = new CodeDeploy({apiVersion: '2014-10-06'})
+const codeDeploy = new CodeDeploy({apiVersion: '2014-10-06'})
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event: any, context: any, callback: any) => {
 
+  const { DeploymentId, LifecycleEventHookExecutionId } = event
+
+  /**
+   * 'Succeeded' or 'Failed'
+   */
+  const STATUS = 'Succeeded'
+
+  /**
+   * Pass AWS CodeDeploy the prepared validation test results.
+   */
+  return await codeDeploy.putLifecycleEventHookExecutionStatus({
+    deploymentId:                  DeploymentId,
+    lifecycleEventHookExecutionId: LifecycleEventHookExecutionId,
+    status:                        STATUS
+  }).promise()
 }
