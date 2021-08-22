d6de79bb789dce5607023e3cbc5a2b7d7218e66d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2021-08-22 14:42:43 +0900

    Fix index

--- afterInstall/src/main/typescript/index.ts
+++ afterInstall/src/main/typescript/index.ts
@@ -3,6 +3,37 @@
  * which is one of the deployment lifecycle event hooks.
  */
 
+import { CodeDeploy } from 'aws-sdk'
+
+const codeDeploy = new CodeDeploy({ apiVersion: '2014-10-06' })
+
 exports.handler = (event: any, context: any, callback: any) => {
 
+  /**
+   * Response CodeDeploy lifecycle event
+   */
+  const { DeploymentId, LifecycleEventHookExecutionId } = event
+
+  /**
+   * 'Succeeded' or 'Failed'
+   */
+  const STATUS = 'Succeeded'
+
+  const params = {
+    deploymentId:                  DeploymentId,
+    lifecycleEventHookExecutionId: LifecycleEventHookExecutionId,
+    status:                        STATUS
+  }
+
+  /**
+   * Pass AWS CodeDeploy the prepared validation test results.
+   */
+  codeDeploy.putLifecycleEventHookExecutionStatus(params, (err, data) => {
+    if (err) { console.log(err) }
+    else {
+      console.log('===============putLifecycleEventHookExecutionStatus===================')
+      console.log(data)
+      callback(null, 'Validation test succeeded')
+    }
+  })
 }
